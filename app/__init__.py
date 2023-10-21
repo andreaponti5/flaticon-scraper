8475cc50f680bc418d4f6019672feb1350ad99d2
Andrea Ponti <andrea.ponti@unimib.it>
2023-10-21 16:24:41 +0200

    Move scraper function to app.py

--- app/__init__.py
+++
@@ -1,1 +0,0 @@
-from .scraper import get_icons_url
--- app/app.py
+++ app/app.py
@@ -7,8 +7,21 @@ import dash_bootstrap_components as dbc
 import requests
 from dash import ALL, html, Output, Input, State, dcc, ctx
 from dash.exceptions import PreventUpdate
+from requests_html import HTMLSession
 
-from scraper import get_icons_url
+
+def get_icons_url(query):
+    url = f"https://www.flaticon.com/search?word={query}"
+    icons_title, icons_url = [], []
+    session = HTMLSession()
+    response = session.get(url)
+    content = response.html.find("div.icon--holder")
+    for icon in content:
+        icons_title.append(icon.find("img", first=True).attrs["alt"])
+        icons_url.append(icon.find("img", first=True).attrs["data-src"])
+    session.close()
+    return icons_url
+
 
 # Initilize the logger
 logging.basicConfig(format='[%(asctime)s] [%(levelname)s]: %(message)s', level=logging.INFO)
